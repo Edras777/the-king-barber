9607a06a594645e9e0c617efa68803600bf271ad
Edras <monterrosoedras@gmail.com>
2024-10-07 17:10:40 -0600

    add home page in package.json

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { NextUIProvider } from "@nextui-org/react";
-import { HashRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <NextUIProvider>
-      <Router>
-        <App />
-      </Router>
+      <App />
     </NextUIProvider>
   </StrictMode>
 );
