6f6572b53dd74091ab60ac8ef8fb8beeab06ccde
Edras <monterrosoedras@gmail.com>
2024-10-07 19:45:45 -0600

    fix environment variable

--- src/services/http.js
+++ src/services/http.js
@@ -3,5 +3,5 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 export default axios.create({
-  baseURL: import.meta.env.CITAS_API_URL ?? "http://localhost:3000",
+  baseURL: import.meta.env.VITE_CITAS_API_URL ?? "http://localhost:3000",
 });
